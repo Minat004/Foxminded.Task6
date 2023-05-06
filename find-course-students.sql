e26708568cdceed5e99eea99a9397ac873fe4683
Minato <umi0n3@gmail.com>
2023-05-06 21:38:32 +0200

    add sql scripts from task

---
+++ find-course-students.sql
@@ -0,0 +1,6 @@
+﻿SELECT Course = c.NAME, [Full name] = st.FIRST_NAME + ' ' + st.LAST_NAME
+FROM db_univer.COURSES c
+JOIN db_univer.GROUPS_COURSES gc ON c.ID = gc.COURSE_ID
+JOIN db_univer.GROUPS gr ON gc.GROUP_ID = gr.ID
+JOIN db_univer.STUDENTS st ON st.GROUP_ID = gr.ID
+ORDER BY c.NAME
