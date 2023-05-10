8a5cc19451e44495918fa761de9c545f1ccffc73
Minato <umi0n3@gmail.com>
2023-05-10 11:28:16 +0200

    course is faculty

--- find-course-students.sql
+++ find-course-students.sql
@@ -1,6 +1,5 @@
 ﻿SELECT Course = c.NAME, [Full name] = st.FIRST_NAME + ' ' + st.LAST_NAME
 FROM db_univer.COURSES c
-JOIN db_univer.GROUPS_COURSES gc ON c.ID = gc.COURSE_ID
-JOIN db_univer.GROUPS gr ON gc.GROUP_ID = gr.ID
-JOIN db_univer.STUDENTS st ON st.GROUP_ID = gr.ID
+JOIN db_univer.GROUPS gr ON c.ID = gr.COURSE_ID
+JOIN db_univer.STUDENTS st ON gr.ID = st.GROUP_ID
 ORDER BY c.NAME
